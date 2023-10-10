951b055fba2940c3fb2a1bbb0b88069b7d637909
Pedro Domínguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2023-10-10 13:32:16 +0200

    Delete dist

--- dist/repository/user.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllUsers = void 0;
-function getAllUsers(pg) {
-    return pg.table('users');
-}
-exports.getAllUsers = getAllUsers;
